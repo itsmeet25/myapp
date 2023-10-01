8a709eee0a3cb11dcb4958e6ea973bbebec744a1
Darshan <darshanpanchal557@gmail.com>
2023-10-01 17:45:12 +0530

    Database connection done

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+require("dotenv").config();
 const helmet = require("helmet");
 const morgan = require("morgan");
 
-mongoose.connect(
-    process.env.MONGO_URL, 
-    { useNewUrlParser: true,useUnifiedTopology: true},
-    () => {
-        console.log("Connected to MongoDB");
-    }
-);
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("DataBase Connected successfully");
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
 
 app.listen(1000, () => {
     console.log("Server is running on port 1000");
